1366bae74f6833e270ee8aa2e1618a5f1d6fe687
rene <rene.fokken@gmail.com>
2024-06-06 01:25:51 +0200

    fixed bot not answering request

--- bot.py
+++ bot.py
@@ -8,21 +8,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client_secret = os.getenv('CLIENT_SECRET')
-server_id = int(os.getenv('SERVER_ID'))
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+SERVER_ID = int(os.getenv('SERVER_ID'))
 
-print(client_secret)
 intents = discord.Intents.all()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
 
-default_map_pool = ["Oregon", "Club House", "Consulate", "Bank", "Kanal", "Chalet", "Kafe Dostoyevsky", "Border",
+DEFAULT_MAP_POOL = ["Oregon", "Club House", "Consulate", "Bank", "Kanal", "Chalet", "Kafe Dostoyevsky", "Border",
                     "Skycraper", "Coastline", "Theme Park", "Villa", "Outback", "Emerald Plains", "Nighthaven Labs",
                     "Lair", "House", "Presidential Plane", "Favela", "Stadium Bravo"]
 
-map_pool = default_map_pool.copy()
+map_pool = DEFAULT_MAP_POOL.copy()
 
 
 def global_map_pool_to_str():
@@ -36,30 +35,32 @@ def global_map_pool_to_str():
 
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=server_id))
+    await tree.sync(guild=discord.Object(id=SERVER_ID))
     print(f'We have logged in as {client.user}')
 
 
-@tree.command(name="pool", description="returns current used map-pool", guild=discord.Object(id=server_id))
-async def first_command(interaction):
-    await interaction.response.send_message(global_map_pool_to_str())
+@tree.command(name="pool", description="returns current used map-pool", guild=discord.Object(id=SERVER_ID))
+async def get_pool(interaction):
+    await interaction.response.defer()
+    await interaction.followup.send(global_map_pool_to_str(), wait=True)
 
 
-@tree.command(name="random", description="returns [number] random maps", guild=discord.Object(id=server_id))
+@tree.command(name="random", description="returns [number] random maps", guild=discord.Object(id=SERVER_ID))
 @app_commands.describe(number="number of maps to return")
 async def random_command(interaction, number: int):
+    await interaction.response.defer()
     global map_pool
 
     if number > len(map_pool):
-        await interaction.response.send_message(f"There are only {len(map_pool)} Maps in the Pool")
+        await interaction.followup.send(f"There are only {len(map_pool)} Maps in the Pool", wait=True)
         return
 
     if number <= 0:
-        await interaction.response.send_message(f"Your Brain must be smoother than Quantum stabilized atom mirror")
+        await interaction.followup.send("Your Brain must be smoother than Quantum stabilized atom mirror", wait=True)
         return
 
     if number == len(map_pool):
-        await interaction.response.send_message(f"Bro wants to select all the Maps💀")
+        await interaction.followup.send("Bro wants to select all the Maps💀", wait=True)
         return
 
     selected_pool = set()
@@ -73,37 +74,39 @@ async def random_command(interaction, number: int):
         out += f"{x + 1} {i}\n"
 
     out += "```"
-    await interaction.response.send_message(out)
+    await interaction.followup.send(out, wait=True)
 
 
-@tree.command(name="map_ban", description="bans one Map from Pool", guild=discord.Object(id=server_id))
+@tree.command(name="map_ban", description="bans one Map from Pool", guild=discord.Object(id=SERVER_ID))
 @app_commands.describe(map_nmbr="number of Maps to return")
 async def map_ban(interaction, map_nmbr: int):
+    await interaction.response.defer()
     global map_pool
 
     if len(map_pool) <= 1:
-        await interaction.response.send_message(f"Selected Map: {map_pool[0]}")
+        await interaction.followup.send(f"Selected Map: {map_pool[0]}", wait=True)
         return
 
     if map_nmbr > len(map_pool):
-        await interaction.response.send_message(f"There are only {len(map_pool)} Maps in the Pool...")
+        await interaction.followup.send(f"There are only {len(map_pool)} Maps in the Pool...", wait=True)
         return
 
     if map_nmbr <= 0:
-        await interaction.response.send_message("0 and smaller is invalid...")
+        await interaction.followup.send("0 and smaller is invalid...", wait=True)
         return
 
     map_pool.pop(map_nmbr - 1)
     if len(map_pool) > 1:
-        await interaction.response.send_message(global_map_pool_to_str())
+        await interaction.followup.send(global_map_pool_to_str(), wait=True)
     else:
-        await interaction.response.send_message(f"Selected Map: {map_pool[0]}")
+        await interaction.followup.send(f"Selected Map: {map_pool[0]}", wait=True)
 
 
-@tree.command(name="reset", description="resets Map Pool to default", guild=discord.Object(id=server_id))
+@tree.command(name="reset", description="resets Map Pool to default", guild=discord.Object(id=SERVER_ID))
 async def reset(interaction):
+    await interaction.response.defer()
     global map_pool
-    map_pool = default_map_pool
-    await interaction.response.send_message("Map Pool is now the default Map Pool")
+    map_pool = DEFAULT_MAP_POOL.copy()
+    await interaction.followup.send("Map Pool is now the default Map Pool", wait=True)
 
-client.run(client_secret)
+client.run(CLIENT_SECRET)
